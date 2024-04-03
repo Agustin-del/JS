38a9ecef5816b7e18a3f34d5f8b4a50e976677f1
Agustin-del <eddyruveda@gmail.com>
2024-04-02 23:24:09 -0300

    fin

--- Recursos Moviestack/scripts/app.js
+++ Recursos Moviestack/scripts/app.js
@@ -1,9 +1,13 @@
 let container = document.getElementById('container')
 
 function createCard(movies) {
+    let template = document.createElement('template')
+    
     for (const movie of movies) {
+        
         let overview = movie.overview.slice(0, 50) + ' ...'
-        container.innerHTML += `<div class = "rounded-md object-contain w-[50%] min-h-fit bg-[#D2CCFF] md:w-1/3 lg:w-1/4 p-3 lg:text-lg">
+        
+        template.innerHTML += `<div class = "rounded-md object-contain w-[50%] min-h-fit bg-[#D2CCFF] md:w-1/3 lg:w-1/4 p-3 lg:text-lg">
         <img class = "rounded-md h-50%"src="${movie.image}" alt="">
         <div class = "gap-2 p-1 flex flex-col">
         <h2 class ="text-center border-2 border-white">${movie.title}</h2>
@@ -12,6 +16,7 @@ function createCard(movies) {
         </div>
         </div>`
     }
+    return container.innerHTML = template.innerHTML
 }
 
 createCard(movies)
