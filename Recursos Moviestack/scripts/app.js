62683c231217eb3932398ddcb38f1f713154a6d4
Agustin-del <eddyruveda@gmail.com>
2024-04-05 08:21:47 -0300

    arreglo funciones y data

--- Recursos Moviestack/scripts/app.js
+++ Recursos Moviestack/scripts/app.js
@@ -1,22 +1,30 @@
 let container = document.getElementById('container')
 
-function createCard(movies) {
+let movies = data.map(movie => movie)
+
+function createCard(movie) {
+    
     let template = document.createElement('template')
     
-    for (const movie of movies) {
+    let overview = movie.overview.slice(0, 50) + ' ...'
         
-        let overview = movie.overview.slice(0, 50) + ' ...'
-        
-        template.innerHTML += `<div class = "rounded-md object-contain w-[50%] min-h-fit bg-[#D2CCFF] md:w-1/3 lg:w-1/4 p-3 lg:text-lg">
-        <img class = "rounded-md h-50%"src="${movie.image}" alt="">
-        <div class = "gap-2 p-1 flex flex-col">
-        <h2 class ="text-center border-2 border-white">${movie.title}</h2>
-        <p>${movie.tagline}</p>
-        <p class = "text-justify">${overview}</p>
-        </div>
-        </div>`
-    }
-    return container.innerHTML = template.innerHTML
+    template.innerHTML += `<div class = "rounded-md object-contain w-[70%] h-[320px] bg-[#D2CCFF] md:w-1/3 lg:w-1/4 p-3 lg:text-lg lg:h-[350px]">
+    <img class = "rounded-md h-[50%]"src="${movie.image}" alt="">
+    <div class = "gap-2 p-1 flex flex-col">
+    <h2 class ="text-center border-2 border-white">${movie.title}</h2>
+    <p>${movie.tagline}</p>
+    <p class = "text-justify text-xs">${overview}</p>
+    </div>
+    </div>`
+
+    return template.innerHTML
 }
 
-createCard(movies)
+function renderCards (movies, container) {
+    for (movie of movies) {
+        container.innerHTML += createCard(movie)
+    }
+    return container   
+}
+
+renderCards(movies, container)
