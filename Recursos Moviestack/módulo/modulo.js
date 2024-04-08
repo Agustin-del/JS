02eec707b68bf4c9e619e98dae48fef14a0c44d2
Agustin-del <eddyruveda@gmail.com>
2024-04-08 19:08:19 -0300

    fin

---
+++ Recursos Moviestack/módulo/modulo.js
@@ -0,0 +1,60 @@
+import { selectedGenre, selectedTitle } from "../scripts/app.js";
+
+export function createCard(movie) {
+    
+    let template = document.createElement('template');
+    
+    let overview = movie.overview.slice(0, 50) + ' ...';
+        
+    template.innerHTML += `<div class = "flex flex-col justify-start rounded-md w-[70%] bg-[#D2CCFF] md:w-1/3 lg:w-1/4 p-3 lg:text-lg">
+    <img class = "rounded-md h-[150px] object-cover" src="${movie.image}" alt="">
+    <div class = "gap-2 p-1 h-[180px] flex flex-col justify-between lg:h-[220px]">
+    <h2 class ="text-center border-2 border-white">${movie.title}</h2>
+    <p>${movie.tagline}</p>
+    <p class = "text-justify text-xs">${overview}</p>
+    <a class = "border-2 text-center" href="./details.html?id=${movie.id}">Ver más</a>
+    </div>
+    </div>`;
+
+    return template.innerHTML;
+}
+
+export function renderCards (movies, container) {
+    for (const movie of movies) {
+        container.innerHTML += createCard(movie);
+    }
+    return container;
+}
+
+export function genres(movies){
+    let genres = new Set();
+
+    movies.forEach(movie => {
+        movie.genres.forEach(genre => {
+            genres.add(genre);
+        });
+    });
+    return [...genres]
+}
+
+export function createOptions(genres, contenedor) {
+
+    for (const genre of genres) {
+        select.innerHTML += `<option value="${genre}">${genre}</option>`;
+    }
+}
+
+export function filterMovies(movies) {
+    return movies.filter(movie => 
+        (selectedGenre === 'all genres' || movie.genres.includes(selectedGenre)) &&
+        movie.title.toLowerCase().includes(selectedTitle.toLowerCase())
+    );
+}
+
+export default {
+    createCard,
+    renderCards,
+    genres,   
+    createOptions,
+    filterMovies   
+}
--- Recursos Moviestack/scripts/app.js
+++ Recursos Moviestack/scripts/app.js
@@ -1,84 +1,35 @@
+import funciones from '../módulo/modulo.js'
+
 let container = document.getElementById('container');
 
 let movies = data.map(movie => movie);
 
-function createCard(movie) {
-    
-    let template = document.createElement('template');
-    
-    let overview = movie.overview.slice(0, 50) + ' ...';
-        
-    template.innerHTML += `<div class = "flex flex-col justify-start rounded-md w-[70%] bg-[#D2CCFF] md:w-1/3 lg:w-1/4 p-3 lg:text-lg">
-    <img class = "rounded-md h-[150px] object-cover" src="${movie.image}" alt="">
-    <div class = "gap-2 p-1 h-[180px] flex flex-col justify-between lg:h-[220px]">
-    <h2 class ="text-center border-2 border-white">${movie.title}</h2>
-    <p>${movie.tagline}</p>
-    <p class = "text-justify text-xs">${overview}</p>
-    <a class = "border-2 text-center" href="./details.html?id=${movie.id}">Ver más</a>
-    </div>
-    </div>`;
-
-    return template.innerHTML;
-}
-
-function renderCards (movies, container) {
-    for (movie of movies) {
-        container.innerHTML += createCard(movie);
-    }
-    return container;
-}
-
-renderCards(movies, container);
-
-function genres(movies){
-    let genres = new Set();
-
-    movies.forEach(movie => {
-        movie.genres.forEach(genre => {
-            genres.add(genre);
-        });
-    });
-    return [...genres]
-}
+funciones.renderCards(movies, container)
 
 let select = document.querySelector('#buscadores select');
 
-function createOptions(genres, contenedor) {
-
-    for (const genre of genres) {
-        select.innerHTML += `<option value="${genre}">${genre}</option>`;
-    }
-}
-
-createOptions(genres(movies, select));
+funciones.createOptions(funciones.genres(movies, select));
 
 let inputText = document.querySelector('#buscadores input')
 
-let selectedGenre = 'all genres';
-let selectedTitle = '';
-
-function filterMovies(movies) {
-    return movies.filter(movie => 
-        (selectedGenre === 'all genres' || movie.genres.includes(selectedGenre)) &&
-        movie.title.toLowerCase().includes(selectedTitle.toLowerCase())
-    );
-}
+export let selectedGenre = 'all genres';
+export let selectedTitle = '';
 
 select.addEventListener('change', event => {
     selectedGenre = event.target.value;
     container.innerHTML = ''
-    renderCards(filterMovies(movies), container);
-});
-
+    funciones.renderCards(funciones.filterMovies(movies), container);
+    console.log(funciones.filterMovies(movies))
+});     
 
 inputText.addEventListener('input', event => {
-    selectedTitle = event.target.value;
+    selectedTitle = event.target.value.toLowerCase().trim();
     container.innerHTML = ''
-    let filteredMovies = filterMovies(movies) 
+    let filteredMovies = funciones.filterMovies(movies) 
     if (filteredMovies.length === 0) {
     container.innerText = 'No one of our movies match your search criteria, sorry.'
     } else {
-    renderCards(filteredMovies, container)
+    funciones.renderCards(filteredMovies, container)
     }
 });
 
@@ -107,6 +58,3 @@ inputText.addEventListener('input', event => {
 //         }
 //     }
 // })
-
-
-// console.log(filteredMovies)
